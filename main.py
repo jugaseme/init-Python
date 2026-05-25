5c1a74a8550707e518c1c7f43016c84036dfc4a8
jugaseme <sebastiangarciamedina26@gmail.com>
2026-05-25 14:11:12 -0500

    primer  python

---
+++ main.py
@@ -0,0 +1,27 @@
+print ("holis")
+#comerntario#
+yo ="sevastian"
+print (yo)
+
+#variables#
+meros = ["hola", "como estas", "bien", "mal"]
+print (meros [0])
+
+#listas#
+jugadores = {
+    "1": "sebastian",
+    "2": "maria"}
+print (jugadores["2"])
+
+#constantes#
+pi = 3.1416
+
+#operadores#
+a = 10
+b = 5
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+print ("suma:", suma)
+print ("resta:", resta)
